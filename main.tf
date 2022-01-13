57d340e6f7ba7c6f87fe7def7b802d2a8c4e9a29
jai <93277391+jayaprakash287@users.noreply.github.com>
2022-01-13 18:28:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 terraform {
-provider "aws" {
 
-    region = "${var.AWS_REGION}"
-}
-    
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
    cloud {
 
     organization = "aws_for_use"
@@ -12,4 +20,8 @@ provider "aws" {
       name = "practive_actions"
     }
   }
+}
+provider "aws" {
+
+    region = "${var.AWS_REGION}"
 }
