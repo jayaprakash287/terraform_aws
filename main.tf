4ab020b183259d3d954cadffed97ba3c2a6a092f
jai <93277391+jayaprakash287@users.noreply.github.com>
2022-01-13 17:57:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+terraform {
 provider "aws" {
 
     region = "${var.AWS_REGION}"
+}
+    
+   cloud {
+
+    organization = "aws_for_use"
+
+    workspaces {
+      name = "practive_actions"
+    }
+  }
 }
